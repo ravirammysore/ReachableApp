92edb9c30c9a05cf9b714dfca05a98835c663829
Ravi K R <raviram.mysore@gmail.com>
2017-10-20 13:04:16 +0530

    some strucural changes

--- app/src/main/java/in/appfocus/reachable/MainActivity.java
+++ app/src/main/java/in/appfocus/reachable/MainActivity.java
@@ -17,52 +17,47 @@ public class MainActivity extends AppCompatActivity {
         if(!Utilities.hasAllPermissions(getApplicationContext()))
             Utilities.requestMissingPermissions(this);
 
+        initSwitches();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        recallSwitchStates();
+    }
+
+    private void initSwitches(){
         swReceiver = (Switch)  findViewById(R.id.swReceiver);
         swAutoResponseSMS = (Switch)  findViewById(R.id.swAutoResponseSMS);
 
         swReceiver.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked)
-                    turnOnReceiver();
-                else
-                    turnOffReceiver();
-
-                //second switch UI state will depend on first switch
-                swAutoResponseSMS.setEnabled(isChecked);
-                swAutoResponseSMS.setChecked(isChecked);
-
+                swReceiverStatusChanged(isChecked);
             }
         });
 
         swAutoResponseSMS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                sendAutoResponseSMS();
+                swAutoResponseSMSStatusChanged(isChecked);
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // TODO: 18-10-2017 check status and update switch states accordingly 
+    private void recallSwitchStates(){
+
     }
 
-    private void turnOnReceiver(){
-        // TODO: 18-10-2017 enable receiver
-        // TODO: 18-10-2017 ask if the user wants to put phone on silent mode 
+    private void swReceiverStatusChanged(Boolean status){
+
     }
 
-    private void turnOffReceiver(){
-        // TODO: 18-10-2017 disable receiver
-        // TODO: 18-10-2017 put phone ringer on normal mode 
+    private void swAutoResponseSMSStatusChanged(Boolean status){
+
     }
 
-    private void sendAutoResponseSMS(){
-        // TODO: 18-10-2017 remember this preference
-    }
-    
     private void putPhoneOnSilentMode(){
         // TODO: 18-10-2017 reduce the ringer volume to min
     }
