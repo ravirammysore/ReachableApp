029408333b4b95619a9be27ce1b994f3a45f9b0a
Ravi K R <raviram.mysore@gmail.com>
2017-11-13 17:48:11 +0530

    now sticky notification show everytime OnReceive which should help in case of a restart bumped build to 3 and version to 1.2

--- app/src/main/java/in/appfocus/reachable/MyReceiver.java
+++ app/src/main/java/in/appfocus/reachable/MyReceiver.java
@@ -2,10 +2,10 @@ package in.appfocus.reachable;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -24,7 +24,6 @@ import static android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION;
  */
 
 public class MyReceiver extends BroadcastReceiver {
-    SharedPreferences sp;
 
     Context context;
     Intent intent;
@@ -33,29 +32,48 @@ public class MyReceiver extends BroadcastReceiver {
 
     String APP_NAME, AUTO_RESPONSE_MESSAGE;
 
-    public MyReceiver() {
-
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        this.context = context;
-        this.intent = intent;
+        //I tried to disable the receiver on shutdown or restart , but din work
 
-        APP_NAME = context.getResources().getString(R.string.app_name);
+        /*
+        *       The following will be received by this broadcast receiver
+        *       Boot is not working, not sure why, but i have it just in case it works!
+        *
+        *       <action android:name="android.intent.action.PHONE_STATE" />
+                <action android:name="android.provider.Telephony.SMS_RECEIVED" />
+                <action android:name="android.intent.action.BOOT_COMPLETED" />
+                <action android:name="android.intent.action.LOCKED_BOOT_COMPLETED" />
+        * */
+        initAll(context,intent);
+
+        //notification needed in case we got here after a reboot! SO that the user gets to know that the app is still armed!
+        //if notification is already being shown, no probs, we ll still have only one notification since the id is the same!
+        showStickyNotification();
 
         String strAction = intent.getAction();
+        Log.d("mytag",strAction);
 
+       //SMS received
         if (strAction.equals(SMS_RECEIVED_ACTION)) {
             smsReceived();
-        } else {
+       }
+        //incoming call action
+        else {
             String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
             //VVIP: we need to respond only to RINGING and not to IDLE or any other states
             if (stateStr != null && !stateStr.isEmpty() && stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                 callReceived();
             }
         }
+    }
+
+    private void initAll(Context context,Intent intent){
+        this.context = context;
+        this.intent = intent;
+
+        APP_NAME = context.getResources().getString(R.string.app_name);
     }
 
     private void smsReceived() {
@@ -74,7 +92,7 @@ public class MyReceiver extends BroadcastReceiver {
 
                 if (strSMS.toLowerCase().contains("urgent") || strSMS.toLowerCase().contains("emergency")) {
                     triggerAlarm();
-                    showNotification();
+                    showNormalNotification();
                 }
             }
         } catch (Exception e) {
@@ -150,7 +168,7 @@ public class MyReceiver extends BroadcastReceiver {
 
     }
 
-    private void showNotification() {
+    private void showNormalNotification() {
 
         NotificationManager notificationManager;
         notificationManager = (NotificationManager)
@@ -164,5 +182,42 @@ public class MyReceiver extends BroadcastReceiver {
 
         Notification notification = builder.build();
         notificationManager.notify(2000, notification);
+    }
+
+   /* private void disableReceiver(){
+        Log.d("mytag","normal mode begin...");
+        Utilities.putPhoneToSNormalMode(context);
+        Log.d("mytag","normal mode done...");
+
+
+        Log.d("mytag","disable receiver  begin...");
+        pm.setComponentEnabledSetting(SMSReceiverComponent,
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
+        Log.d("mytag","disable receiver  done...");
+    }*/
+
+    private void showStickyNotification() {
+        NotificationManager notificationManager;
+        notificationManager = (NotificationManager)
+                context.getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(context, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                1234, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(context)
+                .setContentTitle(APP_NAME)
+                .setContentText("Phone is in " + APP_NAME.toLowerCase() + " mode")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_stat_error_outline)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher));
+
+        Notification notification=builder.build();
+
+        notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+
+        notificationManager.notify(1234, notification);
     }
 }
